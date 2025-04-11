70b879f8f3441780caa5965cef7f5a8fc1ffe0cc
programacaohub7 <flademiroliveira@hotmail.com>
2025-04-11 14:11:38 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,36 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    char estado1 = B; // letra de 'A' a 'H' (representando um dos oito estados).
+ 
+    char codigo1 [04] = B04; // letra do estado seguida de um número de 01 a 04 (ex: A01, B03).
+ 
+    char cidade1 [20] = Florianopolis; //  nome da cidade.
+ 
+    int habitates1 = 580; //  número de habitantes da cidade.
+ 
+    float area1 = 600; // área da cidade em quilômetros quadrados.
+ 
+    float pip1 = 900; // produto Interno Bruto da cidade.
+ 
+    int ponto_turistico = 8; // quantidade de pontos turísticos na cidade.
+
+
+    char estado1 = A; // letra de 'A' a 'H' (representando um dos oito estados).
+ 
+    char codigo1 [02] = B02; // letra do estado seguida de um número de 01 a 04 (ex: A01, B03).
+ 
+    char cidade1 [30] = Joinville; //  nome da cidade.
+ 
+    int habitates1 = 620; //  número de habitantes da cidade.
+ 
+    float area1 = 500; // área da cidade em quilômetros quadrados.
+ 
+    float pip1 = 800; // produto Interno Bruto da cidade.
+ 
+    int ponto_turistico = 9; // quantidade de pontos turísticos na cidade.
+
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
