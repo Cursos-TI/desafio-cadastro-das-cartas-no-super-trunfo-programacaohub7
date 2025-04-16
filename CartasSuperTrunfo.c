cb32b715f93ab83c5d14b16b8bb6d94e43119594
programacaohub7 <flademiroliveira@hotmail.com>
2025-04-16 13:43:19 +0000

    commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,19 +10,28 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
-    char estado1  = 'B'; // letra de 'A' a 'H' (representando um dos oito estados).
+    char estado1  = 'B'; // Letra de 'A' a 'H' (Representando um dos oito estados).
  
-    char codigo1  [04] = "B04"; // letra do estado seguida de um número de 01 a 04 (ex: A01, B03).
+    char codigo1  [04] = "B04"; // Letra do estado seguida de um número de 01 a 04 (ex: A01, B03).
  
-    char cidade1  [20] = "Florianopolis"; //  nome da cidade.
+    char cidade1  [20] = "Florianopolis"; //  Nome da cidade.
  
-    int habitates1  = 1; //  número de habitantes da cidade.
+    int habitates1  = 1000; //  Número de habitantes da cidade.
  
-    float area1  = 1; // área da cidade em quilômetros quadrados.
+    float area1  = 100; // Área da cidade em quilômetros quadrados.
  
-    float pib1  = 1; // produto Interno Bruto da cidade.
+    float PIB1  = 10; // Produto Interno Bruto da cidade.
  
-    int pontos_turisticos1 = 1; //
+    int pontos_turisticos1 = 8; 
+
+    float Densidade_Populacional1 = 50; // Divida a população da cidade pela sua área.
+    // O resultado será a densidade populacional, que representa o número de habitantes por quilômetro quadrado.
+    // Armazene esse valor em uma variável do tipo float.
+
+    float PIB_per_capita1 = 80; // Divida o PIB da cidade pela sua população.
+    // O resultado será o PIB per capita, que indica a riqueza média por pessoa na cidade.
+    // Armazene esse valor em uma variável do tipo float.
+
 
 
     char estado2 = 'A'; // letra de 'A' a 'H' (representando um dos oito estados).
@@ -31,13 +40,22 @@ int main() {
  
     char cidade2 [30] = "Joinville"; //  nome da cidade.
  
-    int habitates2 = 1; //  número de habitantes da cidade.
+    int habitates2 = 1500; //  número de habitantes da cidade.
  
-    float area2 = 1; // área da cidade em quilômetros quadrados.
+    float area2 = 400; // área da cidade em quilômetros quadrados.
  
-    float pib2 = 1; // produto Interno Bruto da cidade.
+    float PIB2 = 300; // produto Interno Bruto da cidade.
  
-    int pontos_turisticos2 = 1; //
+    int pontos_turisticos2 = 6; 
+
+    float Densidade_Populacional2 = 30; // Divida a população da cidade pela sua área.
+    // O resultado será a densidade populacional, que representa o número de habitantes por quilômetro quadrado.
+    // Armazene esse valor em uma variável do tipo float.
+
+    float PIB_per_capita2 = 70; // Divida o PIB da cidade pela sua população.
+    // O resultado será o PIB per capita, que indica a riqueza média por pessoa na cidade.
+    // Armazene esse valor em uma variável do tipo float.
+
 
     
     // Cadastro das Cartas:
@@ -55,12 +73,11 @@ int main() {
     printf("Digite a área da cidade em km²: \n");
     scanf("%f", &area1);
     printf("Digite o PIB da cidade: \n");
-    scanf("%f", &pib1);
-    printf("Digite quantos pontos turisticos existem na cidade: \n");
+    scanf("%f", &PIB1);
+    printf("Digite quantos pontos turisticos existem na cidade: \n \n");
     scanf("%d", &pontos_turisticos1);
 
-
-    printf("Digite uma letra referente ao Estado da Carta: \n");
+    printf("Digite uma letra referente ao estado da carta: \n");
     scanf("%c", &estado2);
     printf("Digite o código da carta: \n");
     scanf("%s", codigo2);
@@ -71,28 +88,30 @@ int main() {
     printf("Digite a área da cidade em km²: \n");
     scanf("%f", &area2);
     printf("Digite o PIB da cidade: \n");
-    scanf("%f", &pib2);
+    scanf("%f", &PIB2);
     printf("Digite quantos pontos turisticos existem na cidade: \n");
     scanf("%d", &pontos_turisticos2);
+    
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    printf("Estado: %c \n", estado1);
-    printf("Código: %s \n", codigo1);
-    printf("Nome da Cidade: %s \n", cidade1);
-    printf("População: %d \n", habitates1);
-    printf("Área: %.2f km² \n", area1);
-    printf("PIB:R$ %.2f \n", pib1);
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("População: %d\n", habitates1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB:R$ %.2f\n", PIB1);
     printf("Número de Pontos Turísticos: %d \n \n",pontos_turisticos1);
-    printf("Estado: %c \n", estado2);
-    printf("Código: %s \n", codigo2);
-    printf("Nome da Cidade: %s \n", cidade2);
-    printf("População: %d \n", habitates2);
-    printf("Área: %.2f km² \n", area2);
-    printf("PIB:R$ %.2f \n", pib2);
-    printf("Número de Pontos Turísticos: %d \n", pontos_turisticos2);
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("População: %d\n", habitates2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB:R$ %.2f\n", PIB2);
+    printf("Número de Pontos Turísticos: %d\n",pontos_turisticos2);
+
 
 
     return 0;
